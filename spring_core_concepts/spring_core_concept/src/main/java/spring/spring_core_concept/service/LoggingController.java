8f425d33d82ce87a101c88ff9bb0b83340cd83a4
unknown <yogeshkushwah775@gmail.com>
2025-02-23 11:10:17 +0530

    [Yogesh Kushwah] Add. Use Built In SLF4J – Simple Logging Framework

--- spring_core_concepts/spring_core_concept/src/main/java/spring/spring_core_concept/service/LoggingController.java
+++ spring_core_concepts/spring_core_concept/src/main/java/spring/spring_core_concept/service/LoggingController.java
@@ -1,19 +1,25 @@
 package spring.spring_core_concept.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
-
+import spring.spring_core_concept.beans.EmployeeBean;
 
 @RestController
 @RequestMapping("/search")
 public class LoggingController {
 
-    //UC01
+    //UC 02
+    //Add Logger to Log Messages Use Built In SLF4J – Simple Logging Framework
+    // Create SLF4J Logger
+    private static final Logger logger = LoggerFactory.getLogger(LoggingController.class);
 
-    @GetMapping("/hello")
-    @ResponseBody
-    public String sayHelloUsingVSCode() {
-        return "Hello from BridgeLabz Using VSCode";
+    // Log message endpoint
+    @GetMapping("/log")
+    public String logExample(@RequestParam(value = "name", defaultValue = "User") String name) {
+        logger.info("Log request received for name: {}", name);
+        return "Hello, " + name + "! Check logs for details.";
     }
-
 
 }
